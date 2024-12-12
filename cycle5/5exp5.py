4db547d3b309d993293a4b325ea5158d2a415f25
Ajin Jose <105265228+Aj12062001@users.noreply.github.com>
2024-12-12 12:06:59 +0530

    Update 5exp5.py

--- cycle5/5exp5.py
+++ cycle5/5exp5.py
@@ -1,7 +1,7 @@
 import graphics.rectangle as rect
 import graphics.circle as circ
-from graphics.volumes import cuboid
-from graphics.volumes import sphere
+from graphics.graphics_3d.cuboid import *
+from graphics.graphics_3d.sphere import *
 
 # Rectangle
 length = int(input("enter the length of the rectangle: "))
@@ -15,13 +15,11 @@ print(f"Circle Area: {circ.area(radius)}")
 print(f"Circle Perimeter (Circumference): {circ.perimeter(radius)}")
 
 # Cuboid
-cuboid_length = int(input("enter the length of the cuboid: "))
-cuboid_width = int(input("enter the width of the cuboid: "))
-cuboid_height = int(input("enter the height of the cuboid: "))
-print(f"Cuboid Surface Area: {cuboid.area(cuboid_length, cuboid_width, cuboid_height)}")
-print(f"Cuboid Volume: {cuboid.volume(cuboid_length, cuboid_width, cuboid_height)}")
+length = int(input("enter the length of the cuboid: "))
+width = int(input("enter the width of the cuboid: "))
+height = int(input("enter the height of the cuboid: "))
+print(f"Cuboid Volume: {volume(length,width,height)}")
 
 # Sphere
 sphere_radius = int(input("enter the radius of the sphere: "))
-print(f"Sphere Surface Area: {sphere.area(sphere_radius)}")
-print(f"Sphere Volume: {sphere.volume(sphere_radius)}")
+print(f"Sphere Volume: {volume1(radius)}")
