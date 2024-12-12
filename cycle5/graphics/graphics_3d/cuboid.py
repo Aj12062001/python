1904be7e80fd9983c592d60ce051b8bcaf9cdec0
Ajin Jose <105265228+Aj12062001@users.noreply.github.com>
2024-12-12 12:07:58 +0530

    Add files via upload

---
+++ cycle5/graphics/graphics_3d/cuboid.py
@@ -0,0 +1,3 @@
+def volume(length,width,height):
+	return length * width * height
+
---
+++ cycle5/graphics/graphics_3d/sphere.py
@@ -0,0 +1,4 @@
+import math
+def volume1(radius):
+    """Calculate the volume of a sphere."""
+    return (4/3) * math.pi * radius ** 3
